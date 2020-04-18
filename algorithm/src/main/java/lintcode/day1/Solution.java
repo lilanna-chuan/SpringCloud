aebd1428197bd15e392bbac84a6b720d4f591eed
QuellanAn <1186154608@qq.com>
2020-04-18 15:03:33 +0800

    lintcode

--- algorithm/src/main/java/lintcode/day1/Solution.java
+++ algorithm/src/main/java/lintcode/day1/Solution.java
@@ -1,6 +1,7 @@
 package lintcode.day1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
@@ -80,5 +81,25 @@ public class Solution {
             return list;
         }
         return list;
+    }
+
+    public int singleNumber(int[] A) {
+        if(A.length==0){
+            return 0;
+        }else if(A.length==1){
+            return A[0];
+        }
+        Arrays.sort(A);
+        if(A[0]!=A[1]){
+            return A[0];
+        }else if(A[A.length-1]!=A[A.length-2]){
+            return A[A.length-1];
+        }
+        for(int i=1;i<A.length-1;i++){
+            if(A[i]!=A[i-1] && A[i]!=A[i+1]){
+                return A[i];
+            }
+        }
+        return 0;
     }
 }
